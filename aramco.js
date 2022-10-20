01b6c52112f1d2c3c419d5bb768919c91ee92ac2
peterbuwa <peterjemiyotan@gmail.com>
2022-10-20 13:06:33 +0100

    first commit

---
+++ aramco.js
@@ -0,0 +1,21 @@
+let hamburger = document.querySelector(".fa-bars");
+let navBar = document.querySelector(".nav-bar");
+let close = document.querySelector(".fa-times")
+let bottomMenuOne = document.querySelector(".bottom-menu-one")
+let bottomMenuTwo = document.querySelector(".bottom-menu-two")
+let bottomMenuThree = document.querySelector(".bottom-menu-three")
+let bottomMenuFour = document.querySelector(".bottom-menu-four")
+let bottomMenuFive = document.querySelector(".bottom-menu-five")
+let bottomMenuSix = document.querySelector(".bottom-menu-six")
+
+hamburger.addEventListener("click", ()=>{
+navBar.style.width = "100%";
+})
+
+close.addEventListener("click", ()=>{
+    navBar.style.width = "0%";
+})
+
+.bottomMenuOne.addEventListener("click", ()=>{
+    
+})
